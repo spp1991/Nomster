b1bd524394ccb7f067a415d9bd209fa59c0dd542
Sunny Patel <spp1991@gmail.com>
2019-08-07 20:24:35 +0000

    Added form for entering details of new place

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,9 @@
 class PlacesController < ApplicationController
   def index
-    @place = Place.all
+    @place = Place.all.page(params[:page]).per_page(5)
+  end
+
+  def new
+    @place = Place.new
   end
 end
