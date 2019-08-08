f7cf4f1c233f437d553957df5fa9b4163a7390b0
Sunny Patel <spp1991@gmail.com>
2019-08-08 02:49:25 +0000

    added gem for user account creation and other reated activities

--- Gemfile
+++ Gemfile
@@ -74,3 +74,4 @@ end
 gem 'will_paginate', '>3.0'
 gem 'simple_form'
 gem "font-awesome-rails"
+gem 'devise'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,7 @@
 Rails.application.configure do
+
+  #instruction as per Devise Gem
+  config.action_mailer.default_url_options = { host: 'nomster-sunny-patel.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
