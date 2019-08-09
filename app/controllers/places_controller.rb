71e745e15994b698a5e87f641a9b03ba4eaa3e2c
Sunny Patel <spp1991@gmail.com>
2019-08-09 02:22:21 +0000

    Added edit place functionality

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -15,6 +15,19 @@ class PlacesController < ApplicationController
 
   end
 
+  def show
+    @place= Place.find(params[:id])
+  end
+
+  def edit
+    @place = Place.find(params[:id])
+  end
+
+  def update
+    @place = Place.find(params[:id])
+    @place.update_attributes(place_params)
+    redirect_to root_path
+  end
   private
 
   def place_params
