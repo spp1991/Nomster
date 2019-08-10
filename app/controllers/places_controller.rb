579cfa4ea6c9d1324403b5c54860505d5eac98c3
Sunny Patel <spp1991@gmail.com>
2019-08-10 18:05:34 +0000

    Added validation

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user! , only: [:new , :create , :edit, :update]
+  before_action :authenticate_user! , only: [:new , :create , :edit, :update, :destroy]
 
   def index
     @place = Place.all.page(params[:page]).per_page(5)
@@ -10,9 +10,13 @@ class PlacesController < ApplicationController
   end
 
   def create
-    current_user.places.create(place_params)
-    redirect_to root_path
-
+    @place=current_user.places.create(place_params)
+    
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :new , status: :unprocessable_entity
+    end
   end
 
   def show
@@ -25,6 +29,8 @@ class PlacesController < ApplicationController
     if @place.user != current_user
       return render plain: "Not Allowed", status: :forbidden  
     end
+
+
   end
 
   def update
@@ -35,11 +41,22 @@ class PlacesController < ApplicationController
     end
 
     @place.update_attributes(place_params)
-    redirect_to root_path
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :edit , status: :unprocessable_entity
+    end
+
+    
   end
 
   def destroy
     @place = Place.find(params[:id])
+    
+    if @place.user != current_user
+      return render plain: "Not Allowed", status: :forbidden  
+    end
+
     @place.destroy
     redirect_to root_path
   end
