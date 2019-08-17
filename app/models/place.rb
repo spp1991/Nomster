761684884e8325f41f27cc7a55bfd2c0a1d3094e
Sunny Patel <spp1991@gmail.com>
2019-08-17 02:32:16 +0000

    included google maps api

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,8 @@
 class Place < ApplicationRecord
   belongs_to :user
-  validates :name , presence: true
+  geocoded_by :address
+  after_validation :geocode
+  
+  validates :name, :address ,:description , presence: true
+  validates :name , :address , :description , length: { minimum: 4}
 end
